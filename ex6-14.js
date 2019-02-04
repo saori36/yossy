64d2b8045cde048f6da9778ec3afebc708a96e2f
zm17454 <zm17454@josai.ac.jp>
2019-02-04 09:36:33 -0500

    message

--- ex6-14.js
+++ ex6-14.js
@@ -6,7 +6,7 @@
   btn.addEventListener('click', function(){
     var n = Math.random()
     if(n < 0.3){
-      this.textContent = '大吉'; // 30%
+      this.textContent = '大吉'; // 30 
     }else if (n < 0.45){
       this.textContent = '吉';  // 15%
     }else if (n < 0.60){
@@ -16,7 +16,6 @@
     }else{
       this.textContent = '凶';  // 15%
     }
-
   });
   btn.addEventListener('mousedown', function(){
     this.className = 'pushed';
